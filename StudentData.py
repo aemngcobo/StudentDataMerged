faf3029cc783c170ab3e3ba9de79adbf7a3fd758
Anele <ngcobo.anele@gmail.com>
2023-07-04 09:55:11 +0200

    Created dictioneries to be merged

---
+++ StudentData.py
@@ -0,0 +1,10 @@
+
+import pandas as pd 
+import openpyxl as xl
+
+data1 = pd.read_excel(r'C:\Users\Anele Ngcobo\Desktop\Basic2AdvancedPythonII\CampaignData\output2.xlsx',sheet_name="X3")
+
+dict1=({'ID':[1000,2222,4433,222],'StudentName':["Amy","Julie","Kia","Jackson"],'Open':[3,2,33,23],'Gender':["Female","Female","NA","Male"]})
+
+dict2=({['ID':[1000,222,443,100],'Qualification':["Grad","Postgrad","Job","Grad"],'Open':[3,112,113,113],'Result'["Pass","Reattempt","Waiting","Pass"]]})
+
