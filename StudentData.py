b2aa357477ab14a5ac4e86f4603d5b3bc6cd0937
Anele <ngcobo.anele@gmail.com>
2023-07-04 10:27:16 +0200

    Created Dataframes from the dictioneries,using Join

--- StudentData.py
+++ StudentData.py
@@ -6,5 +6,12 @@ data1 = pd.read_excel(r'C:\Users\Anele Ngcobo\Desktop\Basic2AdvancedPythonII\Cam
 
 dict1=({'ID':[1000,2222,4433,222],'StudentName':["Amy","Julie","Kia","Jackson"],'Open':[3,2,33,23],'Gender':["Female","Female","NA","Male"]})
 
-dict2=({['ID':[1000,222,443,100],'Qualification':["Grad","Postgrad","Job","Grad"],'Open':[3,112,113,113],'Result'["Pass","Reattempt","Waiting","Pass"]]})
+dict2=({'ID':[1000,222,443,100],'Qualification':["Grad","Postgrad","Job","Grad"],'Open':[3,112,113,113],'Result':["Pass","Reattempt","Waiting","Pass"]})
 
+df1=pd.DataFrame(dict1)
+df2=pd.DataFrame(dict2)
+
+print(df1,df2)
+
+df3=pd.merge(df1,df2,how="inner",on="ID")
+print(df3)
