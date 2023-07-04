56965082af8c4af0406c5983f3faf82495c2b020
Anele <ngcobo.anele@gmail.com>
2023-07-04 12:32:21 +0200

    Merged the dataframes using indexes, (note) the extra row on df2 gets dropped because it doesn't match with any index in df1

--- StudentData.py
+++ StudentData.py
@@ -6,12 +6,13 @@ data1 = pd.read_excel(r'C:\Users\Anele Ngcobo\Desktop\Basic2AdvancedPythonII\Cam
 
 dict1=({'ID':[1000,2222,4433,222],'StudentName':["Amy","Julie","Kia","Jackson"],'Open':[3,2,33,23],'Gender':["Female","Female","NA","Male"]})
 
-dict2=({'Emid':[1000,222,443,100],'Name':["Amy","Matt","Jason","Nikhil"],'Qualification':["Grad","Postgrad","Job","Grad"],'Open':[3,112,113,113],'Result':["Pass","Reattempt","Waiting","Pass"]})
+dict2=({'Emid':[1000,222,443,20,22],'Name':["Amy","Matt","Jason","Nikhil","Kl"],'Qualification':["Grad","Postgrad","Job","Grad","Grad"],'Open':[3,112,113,11,3],'Result':["Pass","Reattempt","Waiting","Pass","P"]})
 
 df1=pd.DataFrame(dict1)
 df2=pd.DataFrame(dict2)
 
-print(df1,df2)
+print(df1)
+print(df2)
 
-df3=pd.merge(df1,df2,how="inner",left_on=["ID","StudentName"], right_on=["Emid","Name"])
+df3=pd.merge(df1,df2, left_index=True,right_index=True)
 print(df3)
